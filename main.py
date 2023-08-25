45ee94c4ad7d2f041038caa5938d2e91b10b8314
Eric Nguyen Van <eric.nguyen_van@onera.fr>
2023-08-25 14:48:54 +0200

    Update validated

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import os.path as pth
 import fastoad.cmd.api as oad
 import warnings
-warnings.filterwarnings(action='ignore', category=UserWarning)
+
 warnings.filterwarnings(action='ignore', category=FutureWarning)
 
 WORK_FOLDER_PATH = pth.join("workdir", "test_dir")
